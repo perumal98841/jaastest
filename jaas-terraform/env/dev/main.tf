062d74d456f4a6ebcbb4a1a063833d7612551d98
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-02 20:16:47 +0530

    Tags update

--- jaas-terraform/env/dev/main.tf
+++ jaas-terraform/env/dev/main.tf
@@ -5,6 +5,16 @@ module "dev_vpc" {
     vpc_id      = "${module.dev_vpc.vpc_id}"
     aws_region = "us-east-1"
     name = "DEV-VPC"
+    business_tags = {
+        BU = "ProductIT"
+        CostCenter = "6465"
+        Owner = "Perumal Varadharajulu"
+        Email = "perumal.varadharajulu@hidglobal.com"
+    }
+    technical_tags = {
+        Env = "dev"
+        Product = "Jenkins-as-a-Service"
+    }
 }
 
 
