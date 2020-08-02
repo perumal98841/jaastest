f72e9493b182cf7a2999a0d7cde82b89f94fee80
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-03 01:27:25 +0530

    script update

--- jaas_scripts/jaas_deploy.sh
+++ jaas_scripts/jaas_deploy.sh
@@ -6,9 +6,9 @@ if [ "$Environment" == "DEV" ]; then
     export AWS_ACCESS_KEY_ID=$DEV_AWS_ACCESS_KEY_ID
 	export AWS_SECRET_ACCESS_KEY=$DEV_AWS_SECRET_ACCESS_KEY
     mv dev.terraform.tfstate terraform.tfstate
-    terraform init jaas-terraform/env/dev
-    terraform plan jaas-terraform/env/dev
-    terraform apply -auto-approve jaas-terraform/env/dev
+    terraform init jaas_terraform/env/dev
+    terraform plan jaas_terraform/env/dev
+    terraform apply -auto-approve jaas_terraform/env/dev
 	ls -ls
     mv terraform.tfstate dev.terraform.tfstate
     cp -f dev.terraform.tfstate ../jaas_destroy_note/.
@@ -25,9 +25,9 @@ else
     export AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESS_KEY_ID
 	export AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRET_ACCESS_KEY
     mv prod.terraform.tfstate terraform.tfstate
-    terraform init jaas-terraform/env/prod
-    terraform plan jaas-terraform/env/prod
-    terraform apply -auto-approve jaas-terraform/env/prod
+    terraform init jaas_terraform/env/prod
+    terraform plan jaas_terraform/env/prod
+    terraform apply -auto-approve jaas_terraform/env/prod
 	ls -ls
 	mv terraform.tfstate prod.terraform.tfstate
     cp -f prod.terraform.tfstate ../jaas_destroy_note/.
--- jaas_scripts/jaas_destroy_note.sh
+++ jaas_scripts/jaas_destroy_note.sh
@@ -6,7 +6,7 @@ if [ "$Environment" == "DEV" ]; then
     export AWS_ACCESS_KEY_ID=$DEV_AWS_ACCESS_KEY_ID
 	export AWS_SECRET_ACCESS_KEY=$DEV_AWS_SECRET_ACCESS_KEY
     mv dev.terraform.tfstate terraform.tfstate
-    terraform destroy -auto-approve jaas-terraform/env/dev
+    terraform destroy -auto-approve jaas_terraform/env/dev
     ls -ls
     mv terraform.tfstate dev.terraform.tfstate
     ls | grep -v dev.terraform.tfstate | grep -v prod.terraform.tfstate | xargs rm -Rf
@@ -21,7 +21,7 @@ else
     export AWS_ACCESS_KEY_ID=$PROD_AWS_ACCESS_KEY_ID
 	export AWS_SECRET_ACCESS_KEY=$PROD_AWS_SECRET_ACCESS_KEY
     mv prod.terraform.tfstate terraform.tfstate
-    terraform destroy -auto-approve jaas-terraform/env/prod
+    terraform destroy -auto-approve jaas_terraform/env/prod
 	ls -ls
 	mv terraform.tfstate prod.terraform.tfstate
     ls | grep -v dev.terraform.tfstate | grep -v prod.terraform.tfstate | xargs rm -Rf
