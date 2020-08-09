9e659ce72be1dbc165ea94978191564f832ae413
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-10 02:59:59 +0530

    keypair issue fix

--- jaas_terraform/env/dev/lc-master.tf
+++ jaas_terraform/env/dev/lc-master.tf
@@ -5,6 +5,6 @@ module "lc_jaas_master" {
     vpc_id              = module.jaas_dev_vpc.vpc_id
     image_id            = "ami-0e942c59119284089"
     instance_type       = "t3a.small"
-    key_name            = module.master_key_pair.this_key_pair_key_pair_id
+    key_name            = module.keypair_master.this_key_pair_key_pair_id
     security_groups     = module.sg_jaas_master.this_security_group_id
 }
