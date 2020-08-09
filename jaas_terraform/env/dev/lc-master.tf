9eb75324cef0e1004247f65467bbbb4fcb036075
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-10 00:02:28 +0530

    lc issue fix

--- jaas_terraform/env/dev/lc-master.tf
+++ jaas_terraform/env/dev/lc-master.tf
@@ -5,8 +5,8 @@ module "lc_jaas_master" {
     vpc_id              = module.jaas_dev_vpc.vpc_id
     image_id            = "ami-0e942c59119284089"
     instance_type       = "t3a.small"
-    key_name            = module.master_key_pair.id
-    security_groups     = module.sg_jaas_master.id
+    key_name            = module.master_key_pair.this_key_pair_key_pair_id
+    security_groups     = module.sg_jaas_master.this_security_group_id
 
     business_tags = {
         BU = "ProductIT"
--- jaas_terraform/modules/launchconfiguration/main.tf
+++ jaas_terraform/modules/launchconfiguration/main.tf
@@ -9,18 +9,6 @@ resource "aws_launch_configuration" "this" {
   user_data                   = var.user_data
   user_data_base64            = var.user_data_base64
   enable_monitoring           = var.enable_monitoring
-  ebs_optimized               = var.ebs_optimized
-
-  dynamic "root_block_device" {
-    for_each = var.root_block_device
-    content {
-      delete_on_termination = lookup(root_block_device.value, "delete_on_termination", null)
-      iops                  = lookup(root_block_device.value, "iops", null)
-      volume_size           = lookup(root_block_device.value, "volume_size", null)
-      volume_type           = lookup(root_block_device.value, "volume_type", null)
-      encrypted             = lookup(root_block_device.value, "encrypted", null)
-    }
-  }
 
   lifecycle {
     create_before_destroy = true
