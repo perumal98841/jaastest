6cdb8a32728a0cd33938a63cd43b1a4ae0a9cdef
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-09 22:35:05 +0530

    Keypair module

---
+++ jaas_terraform/modules/keypair/main.tf
@@ -0,0 +1,15 @@
+resource "aws_key_pair" "this" {
+  count = var.create_key_pair ? 1 : 0
+
+  key_name        = var.key_name
+  key_name_prefix = var.key_name_prefix
+  public_key      = var.public_key
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.key_name)
+    },
+    var.business_tags,
+    var.technical_tags,
+  )
+}
