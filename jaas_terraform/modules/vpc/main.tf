c6493b97a4259fe682a85534de0ef1955a33ee62
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-09 11:57:53 +0530

    Subnet Association

--- jaas_terraform/modules/vpc/main.tf
+++ jaas_terraform/modules/vpc/main.tf
@@ -191,3 +191,21 @@ resource "aws_route" "private_nat_gateway" {
     create = "5m"
   }
 }
+
+#### Route table association ####
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  subnet_id = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(
+    aws_route_table.private.*.id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public[0].id
+}
