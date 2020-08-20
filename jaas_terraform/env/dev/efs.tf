c1648f6838aa18a97d881621e4cc6105adb3e715
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-20 11:33:50 +0530

    EFS tag add

--- jaas_terraform/env/dev/efs.tf
+++ jaas_terraform/env/dev/efs.tf
@@ -5,4 +5,16 @@ module "jaas_efs" {
   vpc_id             = module.jaas_dev_vpc.vpc_id
   subnets            = [module.jaas_dev_vpc.private_subnets[0],module.jaas_dev_vpc.private_subnets[1]]
   security_groups    = module.sg_jaas_efs.this_security_group_id
+
+    business_tags = {
+        BU = "ProductIT"
+        CostCenter = "6465"
+        Owner = "Perumal Varadharajulu"
+        Email = "perumal.varadharajulu@hidglobal.com"
+    }
+    technical_tags = {
+        Env = "dev"
+        Product = "JaaS-Bastion"
+        Terraform = "True"
+    }  
 }
