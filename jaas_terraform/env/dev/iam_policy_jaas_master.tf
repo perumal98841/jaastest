b72d84e81eca25e8b3647bb3ea233e944ad530a2
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-21 13:27:19 +0530

    fix role name

--- jaas_terraform/env/dev/iam_policy_jaas_master.tf
+++ jaas_terraform/env/dev/iam_policy_jaas_master.tf
@@ -11,5 +11,5 @@ module "jaas_iam_policy_master" {
   source = "../../modules/iam_role_policy"
   policy_name = "jaas-master-policy"
   policy = data.aws_iam_policy_document.master_policy.json
-  role = module.jaas_iam_role_master.arn
+  role = module.jaas_iam_role_master.id
 }
