50a02d6c519cd7b5a7dbb58c8cec8683fc845ad6
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-21 16:24:24 +0530

    Update Master AMI

--- jaas_terraform/env/dev/lc_asg.tf
+++ jaas_terraform/env/dev/lc_asg.tf
@@ -10,7 +10,7 @@ module "jaas_lc_asg" {
   # create_lc = false # disables creation of launch configuration
   lc_name = "jaas-dev-lc"
 
-  image_id                     = "ami-0e942c59119284089"
+  image_id                     = "ami-0152172d2e3092ac1"
   instance_type                = "t3a.small"
   key_name                     = module.keypair_master.this_key_pair_key_name
   security_groups              = module.sg_jaas_master.this_security_group_id
