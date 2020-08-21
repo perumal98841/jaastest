84a96ac8744a1b0be18f3d960fc6f5efa6413f5c
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-21 13:33:44 +0530

    Instance profile name

--- jaas_terraform/env/dev/iam_instance_profile_jaas_master.tf
+++ jaas_terraform/env/dev/iam_instance_profile_jaas_master.tf
@@ -1,5 +1,5 @@
 module "jaas_iam_instance_profile" {
   source = "../../modules/iam_instance_profile"
-  profile_name = "jaas-master-profile"
+  profile_name = "jaas-master-instance-profile"
   role = module.jaas_iam_role_master.name
 }
