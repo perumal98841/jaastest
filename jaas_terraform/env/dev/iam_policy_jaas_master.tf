1ae146ccfd4ec8ca5b5fae8fad15e6f004a4de37
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-23 23:09:51 +0530

    update policy

--- jaas_terraform/env/dev/iam_policy_jaas_master.tf
+++ jaas_terraform/env/dev/iam_policy_jaas_master.tf
@@ -1,15 +1,43 @@
 data "aws_iam_policy_document" "master_policy" {
   statement {
     sid       = "AllowSecretManagerAccess"
-    actions   = ["secretsmanager:GetSecretValue",
-                "secretsmanager:ListSecrets",
+    actions   = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:ListSecrets",
     ]
     resources = ["*"]
   }
 }
+
+data "aws_iam_policy_document" "ecr_policy" {
+  statement {
+    sid       = "AllowAccessECR"
+    actions   = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetAuthorizationToken",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:PutImage",
+    ]
+    resources = ["*"]
+  }
+}
+
 module "jaas_iam_policy_master" {
   source = "../../modules/iam_role_policy"
   policy_name = "jaas-master-policy"
   policy = data.aws_iam_policy_document.master_policy.json
+  role = module.jaas_iam_role_master.id
+}
+
+module "jaas_iam_policy_master_ecr" {
+  source = "../../modules/iam_role_policy"
+  policy_name = "jaas-ecr-policy"
+  policy = data.aws_iam_policy_document.ecr_policy.json
   role = module.jaas_iam_role_master.id
 }
