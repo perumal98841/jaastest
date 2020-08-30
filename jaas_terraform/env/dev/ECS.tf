ac50c7d855b1efaf3c6c959092e98035f2e50dba
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-30 20:03:34 +0530

    ELB issue fix

--- jaas_terraform/env/dev/ECS.tf
+++ jaas_terraform/env/dev/ECS.tf
@@ -1,15 +1,15 @@
 module "ECS_jaas_ecs" {
     source      = "../../modules/ecs_fargate"
     aws_region = "us-east-1"
-    app_name = "ECS_JaaS"
-    env_name = "DEV"
+    app_name = "ECS_jaas"
+    env_name = "dev"
     subnets            = [module.jaas_dev_vpc.private_subnets[0],module.jaas_dev_vpc.private_subnets[1]]
     security_groups    = module.sg_jaas_master.this_security_group_id
   
     image_name = "nginx"
     cpu_units         = 256
     ram_units         = 512
-    task_group_family = "test-task-family"
+    task_group_family = "dev-jaas-task-definition"
     container_port    = 80
 
 }
