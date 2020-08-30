75f92d7cc930ec023e893d51a7743a9c6a1ab21b
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-30 14:32:57 +0530

    EC2 ami update

--- jaas_terraform/env/dev/EC2_lc_asg.tf
+++ jaas_terraform/env/dev/EC2_lc_asg.tf
@@ -4,6 +4,7 @@ locals {
 echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-1417b996.efs.us-east-1.amazonaws.com:/ /efs-volume nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
 mount -a -t nfs4
 /opt/nessus_agent/sbin/nessuscli agent link --host=cloud.tenable.com --port=443 --key=bbdb8add4bb0e8f8491c356ab0758ea8eb52c88d9c1102fa72166a83bb7a2ac6 --groups=JaaS-DEV,AWS,HIDG 
+/etc/init.d/jenkins start
 EOF
 }
 
@@ -19,7 +20,7 @@ module "EC2_jaas_lc_asg" {
   # create_lc = false # disables creation of launch configuration
   lc_name = "EC2-jaas-dev-lc"
 
-  image_id                     = "ami-07115dd77b35f0db5"
+  image_id                     = "ami-01a6813e4da4d492b"
   instance_type                = "t3a.small"
   key_name                     = module.keypair_master.this_key_pair_key_name
   security_groups              = module.sg_jaas_master.this_security_group_id
