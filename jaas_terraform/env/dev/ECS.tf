756bc51da63f2803e2e7fed2a3b70da374638a4d
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-08-30 22:23:35 +0530

    task update

--- jaas_terraform/env/dev/ECS.tf
+++ jaas_terraform/env/dev/ECS.tf
@@ -9,7 +9,7 @@ module "ECS_jaas_ecs" {
     image_name = "187945997467.dkr.ecr.us-east-1.amazonaws.com/jaas-dev:jaas-v1.3"
     cpu_units         = 1024
     ram_units         = 2048
-    task_group_family = "dev-jaas-task-definition"
+    task_group_family = "dev-jaas-task"
     container_port    = 8080
     target_group_arn = module.ECS_jaas_dev_alb.target_group_arns[0]
     task_role_arn = module.jaas_iam_role_master.arn
