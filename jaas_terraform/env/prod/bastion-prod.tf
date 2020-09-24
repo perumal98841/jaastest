a50fe94a3f20fff857b392fe90debce45fc7cd84
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-09-25 00:29:02 +0530

    Add bastion

--- jaas_terraform/env/prod/bastion-prod.tf
+++ jaas_terraform/env/prod/bastion-prod.tf
@@ -1,7 +1,7 @@
 module "jaas_prod_bastion_prod" {
     source      = "../../modules/ec2"
 
-    instance_count = 0
+    instance_count = 1
     name          = "JaaS-PROD-Bastion"
   ami           = "ami-0f1319c917f187ba6"
   instance_type = "t3a.small"
