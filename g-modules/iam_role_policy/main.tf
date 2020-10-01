6f5c6423765ca25778dc535458e7b73f6fa0856a
Varadharajulu, Perumal <perumal98841@gmail.com>
2020-10-01 18:42:21 +0530

    Remove Other modules

--- g-modules/iam_role_policy/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_role_policy" "policy" {
-  name        = var.policy_name
-  policy = var.policy
-  role = var.role
-}
--- g-modules/securitygroup/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "this_security_group_id" {
-  description = "The ID of the security group"
-  value = concat(aws_security_group.this.*.id)
-}
-
-output "this_security_group_name" {
-  description = "The Name of the security group"
-  value = concat(aws_security_group.this.*.name)
-}
