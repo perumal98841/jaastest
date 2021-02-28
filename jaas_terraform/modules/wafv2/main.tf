d15b2fa41dd84c429381d47d091894b2fea4c80a
Varadharajulu, Perumal <perumal98841@gmail.com>
2021-02-28 22:12:08 +0530

    WAFv2 module add

---
+++ jaas_terraform/modules/wafv2/main.tf
@@ -0,0 +1,218 @@
+#####
+# WAFv2 web acl
+#####
+resource "aws_wafv2_web_acl" "main" {
+  count = var.enabled ? 1 : 0
+
+  name  = var.name_prefix
+  scope = var.scope
+
+  default_action {
+    dynamic "allow" {
+      for_each = var.allow_default_action ? [1] : []
+      content {}
+    }
+
+    dynamic "block" {
+      for_each = var.allow_default_action ? [] : [1]
+      content {}
+    }
+  }
+
+  dynamic "rule" {
+    for_each = var.rules
+    content {
+      name     = lookup(rule.value, "name")
+      priority = lookup(rule.value, "priority")
+
+      override_action {
+        dynamic "none" {
+          for_each = length(lookup(rule.value, "override_action", {})) == 0 || lookup(rule.value, "override_action", {}) == "none" ? [1] : []
+          content {}
+        }
+
+        dynamic "count" {
+          for_each = lookup(rule.value, "override_action", {}) == "count" ? [1] : []
+          content {}
+        }
+      }
+
+      statement {
+        dynamic "managed_rule_group_statement" {
+          for_each = length(lookup(rule.value, "managed_rule_group_statement", {})) == 0 ? [] : [lookup(rule.value, "managed_rule_group_statement", {})]
+          content {
+            name        = lookup(managed_rule_group_statement.value, "name")
+            vendor_name = lookup(managed_rule_group_statement.value, "vendor_name", "AWS")
+
+            dynamic "excluded_rule" {
+              for_each = length(lookup(managed_rule_group_statement.value, "excluded_rule", {})) == 0 ? [] : toset(lookup(managed_rule_group_statement.value, "excluded_rule"))
+              content {
+                name = excluded_rule.value
+              }
+            }
+          }
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = length(lookup(rule.value, "visibility_config")) == 0 ? [] : [lookup(rule.value, "visibility_config", {})]
+        content {
+          cloudwatch_metrics_enabled = lookup(visibility_config.value, "cloudwatch_metrics_enabled", true)
+          metric_name                = lookup(visibility_config.value, "metric_name", "${var.name_prefix}-default-rule-metric-name")
+          sampled_requests_enabled   = lookup(visibility_config.value, "sampled_requests_enabled", true)
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = var.ip_set_rules
+    content {
+      name     = lookup(rule.value, "name")
+      priority = lookup(rule.value, "priority")
+
+      action {
+        dynamic "allow" {
+          for_each = length(lookup(rule.value, "action", {})) == 0 || lookup(rule.value, "action", {}) == "allow" ? [1] : []
+          content {}
+        }
+
+        dynamic "count" {
+          for_each = lookup(rule.value, "action", {}) == "count" ? [1] : []
+          content {}
+        }
+
+        dynamic "block" {
+          for_each = lookup(rule.value, "action", {}) == "block" ? [1] : []
+          content {}
+        }
+      }
+
+      statement {
+        dynamic "ip_set_reference_statement" {
+          for_each = length(lookup(rule.value, "ip_set_reference_statement", {})) == 0 ? [] : [lookup(rule.value, "ip_set_reference_statement", {})]
+          content {
+            arn = lookup(ip_set_reference_statement.value, "arn")
+          }
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = length(lookup(rule.value, "visibility_config")) == 0 ? [] : [lookup(rule.value, "visibility_config", {})]
+        content {
+          cloudwatch_metrics_enabled = lookup(visibility_config.value, "cloudwatch_metrics_enabled", true)
+          metric_name                = lookup(visibility_config.value, "metric_name", "${var.name_prefix}-ip-rule-metric-name")
+          sampled_requests_enabled   = lookup(visibility_config.value, "sampled_requests_enabled", true)
+        }
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = var.ip_rate_based_rule != null ? [var.ip_rate_based_rule] : []
+    content {
+      name     = lookup(rule.value, "name")
+      priority = lookup(rule.value, "priority")
+
+      action {
+        dynamic "count" {
+          for_each = lookup(rule.value, "action", {}) == "count" ? [1] : []
+          content {}
+        }
+
+        dynamic "block" {
+          for_each = length(lookup(rule.value, "action", {})) == 0 || lookup(rule.value, "action", {}) == "block" ? [1] : []
+          content {}
+        }
+      }
+
+      statement {
+        dynamic "rate_based_statement" {
+          for_each = length(lookup(rule.value, "rate_based_statement", {})) == 0 ? [] : [lookup(rule.value, "rate_based_statement", {})]
+          content {
+            limit              = lookup(rate_based_statement.value, "limit")
+            aggregate_key_type = lookup(rate_based_statement.value, "aggregate_key_type", "IP")
+
+            dynamic "forwarded_ip_config" {
+              for_each = length(lookup(rule.value, "forwarded_ip_config", {})) == 0 ? [] : [lookup(rule.value, "forwarded_ip_config", {})]
+              content {
+                fallback_behavior = lookup(forwarded_ip_config.value, "fallback_behavior")
+                header_name       = lookup(forwarded_ip_config.value, "header_name")
+              }
+            }
+          }
+        }
+      }
+
+      dynamic "visibility_config" {
+        for_each = length(lookup(rule.value, "visibility_config")) == 0 ? [] : [lookup(rule.value, "visibility_config", {})]
+        content {
+          cloudwatch_metrics_enabled = lookup(visibility_config.value, "cloudwatch_metrics_enabled", true)
+          metric_name                = lookup(visibility_config.value, "metric_name", "${var.name_prefix}-ip-rate-based-rule-metric-name")
+          sampled_requests_enabled   = lookup(visibility_config.value, "sampled_requests_enabled", true)
+        }
+      }
+    }
+  }
+
+  tags = var.tags
+
+  dynamic "visibility_config" {
+    for_each = length(var.visibility_config) == 0 ? [] : [var.visibility_config]
+    content {
+      cloudwatch_metrics_enabled = lookup(visibility_config.value, "cloudwatch_metrics_enabled", true)
+      metric_name                = lookup(visibility_config.value, "metric_name", "${var.name_prefix}-default-web-acl-metric-name")
+      sampled_requests_enabled   = lookup(visibility_config.value, "sampled_requests_enabled", true)
+    }
+  }
+}
+
+#####
+# WAFv2 web acl association with ALB
+#####
+resource "aws_wafv2_web_acl_association" "main" {
+  count = var.enabled && var.create_alb_association && length(var.alb_arn_list) == 0 ? 1 : 0
+
+  resource_arn = var.alb_arn
+  web_acl_arn  = aws_wafv2_web_acl.main[0].arn
+
+  depends_on = [aws_wafv2_web_acl.main]
+}
+
+resource "aws_wafv2_web_acl_association" "alb_list" {
+  count = var.enabled && var.create_alb_association && length(var.alb_arn_list) > 0 ? length(var.alb_arn_list) : 0
+
+  resource_arn = var.alb_arn_list[count.index]
+  web_acl_arn  = aws_wafv2_web_acl.main[0].arn
+
+  depends_on = [aws_wafv2_web_acl.main]
+}
+
+#####
+# WAFv2 web acl logging configuration with kinesis firehose
+#####
+resource "aws_wafv2_web_acl_logging_configuration" "main" {
+  count = var.enabled && var.create_logging_configuration ? 1 : 0
+
+  log_destination_configs = var.log_destination_configs
+  resource_arn            = aws_wafv2_web_acl.main[0].arn
+
+  dynamic "redacted_fields" {
+    for_each = var.redacted_fields
+    content {
+      dynamic "single_header" {
+        for_each = length(lookup(redacted_fields.value, "single_header", {})) == 0 ? [] : [lookup(redacted_fields.value, "single_header", {})]
+        content {
+          name = lookup(single_header.value, "name", null)
+        }
+      }
+
+      dynamic "single_query_argument" {
+        for_each = length(lookup(redacted_fields.value, "single_query_argument", {})) == 0 ? [] : [lookup(redacted_fields.value, "single_query_argument", {})]
+        content {
+          name = lookup(single_query_argument.value, "name", null)
+        }
+      }
+    }
+  }
+}
