7db280a4b9f4775255ade64956971f61fe5cd482
Varadharajulu, Perumal <perumal98841@gmail.com>
2021-03-11 23:47:17 +0530

    Update Regex

--- jaas_terraform/modules/wafv2/outputs.tf
+++ jaas_terraform/modules/wafv2/outputs.tf
@@ -18,7 +18,3 @@ output "web_acl_capacity" {
   value       = join("", aws_wafv2_web_acl.main.*.capacity)
 }
 
-output "regex_pattern_set_arn" {
-  description = "The ARN of the regex_pattern_set."
-  value       = join("", aws_wafv2_regex_pattern_set.jaas-regex.*.arn)
-}
