426ca99ce1131070e494eaaee2e5a48369647613
Varadharajulu, Perumal <perumal98841@gmail.com>
2021-05-22 15:25:21 +0530

    update name

--- jaas_terraform/env/dev/jaas-master-log-group.tf
+++ jaas_terraform/env/dev/jaas-master-log-group.tf
@@ -1,5 +1,5 @@
-module "jaas_master_dev_log_group" {
+module "jaas_dev_master_log_group" {
   source      = "../../modules/log-group"
-  name              = "jaas_master-dev-log"
+  name              = "jaas-dev-master-log-us-east-1"
   retention_in_days = 15
 }
