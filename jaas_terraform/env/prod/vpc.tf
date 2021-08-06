97c7effa47f6fdf6ba8062a0ef33d45c09cd265d
Varadharajulu, Perumal <perumal98841@gmail.com>
2021-08-06 14:38:27 +0530

    Enable single NAT gateway

--- jaas_terraform/env/prod/vpc.tf
+++ jaas_terraform/env/prod/vpc.tf
@@ -27,8 +27,8 @@ module "jaas_prod_vpc" {
     private_subnets = ["10.8.25.32/28","10.8.25.48/28","10.8.25.96/27","10.8.25.128/27","10.8.25.160/28","10.8.25.176/28"]
 
     enable_nat_gateway = true
-    single_nat_gateway = true
-    one_nat_gateway_per_az = false
+    single_nat_gateway = false
+    one_nat_gateway_per_az = true
     reuse_nat_ips = true
     external_nat_ip_ids = "${aws_eip.nat.*.id}"    
     business_tags = {
