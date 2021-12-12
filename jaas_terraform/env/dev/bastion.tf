09183c5cf80bfbe43b0c0b2f6570dbf1b2a98105
Varadharajulu, Perumal <perumal98841@gmail.com>
2021-12-12 13:22:36 +0530

    Update Bastion

--- jaas_terraform/env/dev/bastion.tf
+++ jaas_terraform/env/dev/bastion.tf
@@ -3,7 +3,7 @@ module "jaas_dev_bastion" {
 
     instance_count = 1
     name          = "perumal-JaaS-DEV-Bastion"
-  ami           = "ami-0f1319c917f187ba6"
+  ami           = "ami-02404348e17201e00"
   instance_type = "t3a.small"
   subnet_id     = tolist(module.jaas_dev_vpc.public_subnets)[0]
   private_ips                 = ["10.3.170.14"]
