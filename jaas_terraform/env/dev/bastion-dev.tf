4eb3bbdf2f45f10e21fb9462552723a6a57f0a95
Perumal Varadharajulu <perumal98841@gmail.com>
2023-09-28 00:51:07 +0530

    Update

--- jaas_terraform/env/dev/bastion-dev.tf
+++ jaas_terraform/env/dev/bastion-dev.tf
@@ -1,12 +1,12 @@
 module "jaas_dev_bastion_dev" {
     source      = "../../modules/ec2"
 
-    instance_count = 0
+    instance_count = 1
     name          = "JaaS-DEV-Bastion-PrivX"
   ami           = "ami-02404348e17201e00"
   instance_type = "t3a.medium"
   subnet_id     = tolist(module.jaas_dev_vpc.private_subnets)[1]
-  private_ips                 = ["10.3.170.62"]
+  #private_ips                 = ["10.3.170.62"]
   user_data_base64 = "PHNjcmlwdD4KbXNpZXhlYyAvaSBjOlxOZXNzdXNBZ2VudFxOZXNzdXNBZ2VudC5tc2kgTkVTU1VTX0dST1VQUz0iSmFhUy1ERVYsQVdTLEhJREciIE5FU1NVU19TRVJWRVI9ImNsb3VkLnRlbmFibGUuY29tOjQ0MyIgTkVTU1VTX0tFWT1iYmRiOGFkZDRiYjBlOGY4NDkxYzM1NmFiMDc1OGVhOGViNTJjODhkOWMxMTAyZmE3MjE2NmE4M2JiN2EyYWM2IC9xbgo8L3NjcmlwdD4="  
   vpc_security_group_ids      =   module.sg_jaas_bastion_dev.this_security_group_id
     business_tags = {
