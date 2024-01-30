0a567eb1b50065da5c2cc6255d8a8f7868d76a91
Perumal Varadharajulu <perumal98841@gmail.com>
2024-01-30 23:07:36 +0530

    AMI update

--- jaas_terraform/env/dev/lc_asg.tf
+++ jaas_terraform/env/dev/lc_asg.tf
@@ -21,8 +21,8 @@ module "jaas_lc_asg" {
   # launch_configuration = "my-existing-launch-configuration" # Use the existing launch configuration
   # create_lc = false # disables creation of launch configuration
   lc_name = "jaas-dev-lc"
-  image_id                     = "ami-0c7f2cc2e71e900c3"
-  # OLD image = image_id                     = "ami-073ff107ef7ac6813"
+  image_id                     = "ami-0352b86d25e5cf435"
+  # OLD image = image_id                     = "ami-0c7f2cc2e71e900c3"
   instance_type                = "t3a.small"
   #key_name                     = module.keypair_master.this_key_pair_key_name
   key_name                     = "perumal-ubuntu"
